bc97b097bc74c15801dd93a1666e7c2cf6036eb9
Ibrahim Rmili <brahimremili041@gmail.com>
2025-06-20 20:12:51 +0100

    first commit

---
+++ details.js
@@ -0,0 +1,15 @@
+// details.js
+fetch('foods.json')
+  .then(response => response.json())
+  .then(data => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = parseInt(urlParams.get('id'), 10);
+    const item = data.find(d => d.id === id);
+
+    if (item) {
+      document.getElementById('food-image').src = item.image;
+      document.getElementById('food-title').textContent = item.name;
+      document.getElementById('food-price').textContent = "$"+item.price;
+      document.getElementById('food-description').textContent = item.description;
+    }
+  });
